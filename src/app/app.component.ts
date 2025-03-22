dfa3a373599a3abd454e85399fc0b0bf20d7b768
chathuraruwantha-2002 <143559027+chathuraruwantha-2002@users.noreply.github.com>
2025-03-22 13:12:45 +0530

    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent implements OnInit {
+  title = 'StudyMate-Quiz-backend';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+}
